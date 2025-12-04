4e9264e45df23daf72985c1537f20b53cd7a5718
hmayorgav <hmayorgav@unal.edu.co>
2025-12-04 15:44:19 -0500

    aaaaa

--- fibo.py
+++ fibo.py
@@ -15,7 +15,7 @@ def fib_recursive(n: int) -> int:
 
 	if n < 2:
 		return n
-	return fib_recursive(n - 1) + fib_recursive(n - 2)
+	return fib_recursive(n) + fib_recursive(n - 3)
 
 
 def fib_iterative(n: int) -> int:
